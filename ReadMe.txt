0ffc29679e866c17b7778b19bb35f47e7d08e1e1
Brandon Ko <brandonko@Brandons-MacBook-Pro-2.local>
2018-06-25 15:48:33 -0700

    1.2

--- ReadMe.txt
+++ ReadMe.txt
@@ -6,6 +6,12 @@ I created this extension to provide more in-depth statistics on grades as well a
 calculate the minimum score on future assignments to maintain a specific grade.
 This is made for UW students who have grades canvas.uw.edu.
 
+-Version 1.0-
+Weighted calculation almost fully functional.
+
 -Version 1.1-
 Working weighted and unweighted grade calculator. Still need to provide the table
 with editing option to edit each individual score per category (increase customizability).
+
+-Version 1.2-
+Added reset button to clear data after selecting "Weighted" or "Unweighted". Changed "Make an Edit" to edit the Grade Categories table by category instead of adjusting the total acquired points directly. More specific, and makes weighted calculations possible after edits. Added categories to minimum score, allowing weighted calculations to work for minimum score as well.
--- popup.js
+++ popup.js
@@ -10,7 +10,8 @@ window.onload = function () {
 	function $(id) {return document.getElementById(id)}
 	function cl(e) {console.log(e)}
 	function $cn(id) {return document.getElementsByClassName(id)}
-	function clStr(content) {chrome.tabs.executeScript({code: "cl('" + content + "')"})}
+	function clStr(content) {chrome.tabs.executeScript({
+		code: "cl('" + content + "')"})}
 	function round100(num) {return Math.round(num * 100) / 100}
 
 	var acquiredPoints;
@@ -19,32 +20,70 @@ window.onload = function () {
 	var gradePercentageMean;
 	var totalPoints;
 	var processedStats;
+	var weighted;
+	var weightedGradeValues = {};
 
 	//Extracts the data when the popup is opened
 	chrome.tabs.executeScript({file: 'extractData.js'});
 
 	//Updates Statistics table with current variables
 	function updateStatistics() {
-		if (!isNaN(acquiredPoints)){$('acquiredPoints').innerHTML =
-			Math.round(acquiredPoints*100)/100};
-		if (!isNaN(gradePercentage)){$('gradePercentage').innerHTML =
-			Math.round(gradePercentage*100)/100};
-		if (!isNaN(acquiredPointsMean)){$('acquiredPointsMean').innerHTML =
-			Math.round(acquiredPointsMean*100)/100};
-		if (!isNaN(gradePercentageMean)){$('gradePercentageMean').innerHTML =
-			Math.round(gradePercentageMean*100)/100};
-		if (!isNaN(totalPoints)){$('totalPoints').innerHTML =
-			Math.round(totalPoints*100)/100};
+		if (!isNaN(acquiredPoints)) {
+			$('acquiredPoints').innerHTML =
+			Math.round(acquiredPoints*100)/100;
+		} else {
+			$('acquiredPoints').innerHTML = "--"
+		}
+		if (!isNaN(gradePercentage)) {
+			$('gradePercentage').innerHTML =
+			Math.round(gradePercentage*100)/100;
+		} else {
+			$('gradePercentage').innerHTML = "--";
+		}
+		if (!isNaN(acquiredPointsMean)) {
+			$('acquiredPointsMean').innerHTML =
+			Math.round(acquiredPointsMean*100)/100;
+		} else {
+			$('acquiredPointsMean').innerHTML = "--";
+		}
+		if (!isNaN(gradePercentageMean)) {
+			$('gradePercentageMean').innerHTML =
+			Math.round(gradePercentageMean*100)/100;
+		} else {
+			$('gradePercentageMean').innerHTML = "--";
+		}
+		if (!isNaN(totalPoints)) { 
+			$('totalPoints').innerHTML =
+			Math.round(totalPoints*100)/100;
+		} else {
+			$('totalPoints').innerHTML = "--";
+		}
 	}
+
+	//Resets Popup
+	$('resetStatistics').addEventListener('click', function() {
+		acquiredPoints = undefined;
+		gradePercentage = undefined;
+		acquiredPointsMean = undefined;
+		gradePercentageMean = undefined;
+		totalPoints = undefined;
+		processedStats = undefined;
+		updateStatistics();
+		$('weightedPercentages').innerHTML = "";
+		$('categoricalGrades').innerHTML = "";
+		$('editForm').innerHTML = "";
+		$('minScoreTable').innerHTML = "";
+		$('statsTitle').innerHTML = "Statistics";
+		clStr("Local data has been reset")
+	})
 
 	//Collect data for an unweighted course
 	$('unweightedCalc').addEventListener('click', function() {
-
-		//chrome.tabs.executeScript({file: 'getScoresUnweighted.js'});
+		$('weightedPercentages').innerHTML = "";
 		$('statsTitle').innerHTML = "Statistics (unweighted)";
-
+		weighted = false;
 		chrome.storage.sync.get(['processedStats'], function(e) {
-			createTable();
+			processedStats = e.processedStats;
 			acquiredPoints = 0;
 			totalPoints = 0;
 			acquiredPointsMean = 0;
@@ -57,13 +96,16 @@ window.onload = function () {
 			gradePercentageMean = acquiredPointsMean/totalPoints * 100;
 			createTable();
 			updateStatistics();
+			createEditForm();
 		})
-
 	})
 
 	//Collect data for an weighted course 
 	$('weightedCalc').addEventListener('click', function() {
+		$('statsTitle').innerHTML = "Statistics (weighted)"
+		weighted = true;
 		chrome.storage.sync.get(['processedStats'], function(e) {
+			processedStats = e.processedStats;
 			acquiredPoints = 0;
 			totalPoints = 0;
 			acquiredPointsMean = 0;
@@ -74,8 +116,9 @@ window.onload = function () {
 			}
 			createTable();
 			updateStatistics();
+			createEditForm();
 
-			//TODO: add table weight input (with button)
+			//Creates table for weight input
 			$('weightedPercentages').innerHTML = "";
 			var table = document.createElement("TABLE")
 			var row = document.createElement("TR")
@@ -96,7 +139,7 @@ window.onload = function () {
 				var input = document.createElement('INPUT');
 				input.classList.add("gradeInput")
 				input.type = "number"
-				input.id = key
+				input.id = key + "Weight"
 				row.appendChild(input)
 				var e2 = document.createElement("TD")
 				var txt2 = document.createTextNode("%")
@@ -110,13 +153,14 @@ window.onload = function () {
 			button.id = "submitWeightedPercentages"
 			$('weightedPercentages').appendChild(button)
 
+
 			//Calculate grade percentages based on weighted values submitted by user
 			$('submitWeightedPercentages').addEventListener('click', function() {
 				chrome.storage.sync.get(['processedStats'], function(e) {
 					gradePercentage = 0;
 					gradePercentageMean = 0;
 					for (var key in e.processedStats) {
-						var weight = $(key).value/100
+						var weight = $(key + "Weight").value/100
 						var myGrade = e.processedStats[key].myScore /
 						e.processedStats[key].totalPoints;
 						var meanGrade = e.processedStats[key].mean /
@@ -124,18 +168,59 @@ window.onload = function () {
 
 						gradePercentage += weight * myGrade
 						gradePercentageMean += weight * meanGrade
+						weightedGradeValues[key] = {"gradePercentage" : weight * myGrade, 
+						"gradePercentageMean" : weight * meanGrade, 
+						"weight" : weight}
 					}
 					gradePercentage *= 100
 					gradePercentageMean *= 100
+					clStr(JSON.stringify(weightedGradeValues))
 					updateStatistics();
 				})
 			})
 		})
 	})
 
+	//Create form for editing grades and min score calculation
+	function createEditForm() {
+		$('editForm').innerHTML = ""
+		$('minScoreTable').innerHTML = ""
+
+		var table = document.createElement("TABLE")
+		for (var key in processedStats) {
+			var row = document.createElement("TR")
+			var e1 = document.createElement("TD")
+			var inp = document.createElement("INPUT")
+			inp.type = "radio"
+			inp.name = "categoryEdit"
+			inp.id = key + "button"
+			e1.appendChild(inp)
+			var e2 = document.createElement("TD")
+			e2.appendChild(document.createTextNode(key))
+			row.appendChild(e1)
+			row.appendChild(e2)
+			table.appendChild(row)
+		}
+		$('editForm').appendChild(table)
+
+		for (var key in processedStats) {
+			var row = document.createElement("TR")
+			var e1 = document.createElement("TD")
+			var inp = document.createElement("INPUT")
+			inp.type = "radio"
+			inp.name = "categoryEdit"
+			inp.id = key + "minScoreButton"
+			e1.appendChild(inp)
+			var e2 = document.createElement("TD")
+			e2.appendChild(document.createTextNode(key))
+			row.appendChild(e1)
+			row.appendChild(e2)
+			$('minScoreTable').appendChild(row)
+		}
+	}
 
 
-	//Creates the data table
+	//Create table with points in each grade category
 	function createTable() {
 		chrome.storage.sync.get(['processedStats'], function(e) {
 			$('categoricalGrades').innerHTML = "";
@@ -174,6 +259,7 @@ window.onload = function () {
 				var e1 = document.createElement("TD")
 				if (e.processedStats[key] != undefined) {
 					var text1 = document.createTextNode(round100(e.processedStats[key].mean))
+					e1.id = key + "meanTable"
 					e1.appendChild(text1);
 				} else {
 					e1.appendChild(document.createTextNode("--"))
@@ -181,12 +267,14 @@ window.onload = function () {
 				row1.appendChild(e1);
 
 				var e2 = document.createElement("TD")
+				e2.id = key + "myScoreTable"
 				var text2 = document.createTextNode(round100(e.processedStats[key].myScore))
 				e2.appendChild(text2);
 				row1.appendChild(e2);
 
 				var e3 = document.createElement("TD")
 				var text3 = document.createTextNode(round100(e.processedStats[key].totalPoints))
+				e3.id = key + "totalPointsTable"
 				e3.appendChild(text3);
 				row1.appendChild(e3);
 
@@ -198,41 +286,59 @@ window.onload = function () {
 
 	//Make an edit to Statistics
 	$('submitEdit').addEventListener('click', function() {
-		var mod = parseFloat(gradeEdit.value);
-		var element;
-		var curNum;
-
-		if (!isNaN(mod)) {
-			if ($('subtractRadio').checked) {
-				mod = -mod;
-			}
-
-			if ($('myGradesRadio').checked && $('percentageRadio').checked) {
-				element = $('gradePercentage');
-				gradePercentage += mod;
-				acquiredPoints = totalPoints * gradePercentage / 100;
-			} else if ($('myGradesRadio').checked && $('pointsRadio').checked
-				&& !isNaN(acquiredPoints)) {
-				element = $('acquiredPoints');
-				acquiredPoints += mod;
-				gradePercentage = acquiredPoints / totalPoints * 100;
-			} else if ($('classGradesRadio').checked && $('percentageRadio').checked) {
-				element = $('gradePercentageMean');
-				gradePercentageMean += mod;
-				acquiredPointsMean = totalPoints * gradePercentageMean / 100;
-			} else if ($('classGradesRadio').checked && $('pointsRadio').checked
-				&& !isNaN(acquiredPointsMean)) {
-				element = $('acquiredPointsMean');
-				acquiredPointsMean += mod;
-				gradePercentageMean = acquiredPointsMean / totalPoints * 100;
-			} else if ($('totalPointsRadio').checked) {
-				element = $('totalPoints');
-				totalPoints += mod;
-				gradePercentage = acquiredPoints / totalPoints * 100;
-				gradePercentageMean = acquiredPointsMean / totalPoints * 100;
-			}
-			updateStatistics()
+		clStr("begin edit")
+		var category;
+		var mod = parseFloat($('gradeEdit').value);
+		if (isNaN(mod)) {
+			mod = 0
 		}
+		var column;
+		if ($('sub').checked) mod = -mod;
+		for (var key in processedStats) {
+			if ($(key + "button").checked) {
+				category = key
+			}
+		}
+		if ($('myGradesRadio').checked) {
+			column = "myScore"
+		} else if ($('classMeanRadio').checked) {
+			column = "mean"
+		} else {
+			column = "totalPoints"
+		}
+		processedStats[category][column] += mod
+
+		var id = category + column + "Table"
+		$(id).innerHTML = processedStats[category][column]
+
+		if (weighted) {
+			gradePercentage = 0;
+			gradePercentageMean = 0;
+			for (var key in processedStats) {
+				var weight = weightedGradeValues[key].weight
+				var myGrade = processedStats[key].myScore /
+				processedStats[key].totalPoints;
+				var meanGrade = processedStats[key].mean /
+				processedStats[key].totalPoints;
+
+				gradePercentage += weight * myGrade
+				gradePercentageMean += weight * meanGrade
+			}
+			gradePercentage *= 100
+			gradePercentageMean *= 100
+		} else {
+			acquiredPoints = 0;
+			totalPoints = 0;
+			acquiredPointsMean = 0;
+			for(var key in processedStats) {
+				acquiredPoints += processedStats[key].myScore;
+				totalPoints += processedStats[key].totalPoints;
+				acquiredPointsMean += processedStats[key].mean;
+			}
+			gradePercentage = acquiredPoints/totalPoints * 100;
+			gradePercentageMean = acquiredPointsMean/totalPoints * 100;
+		}
+		updateStatistics();
 	})
 
 
@@ -240,9 +346,29 @@ window.onload = function () {
 	$('unweightedMinScore').addEventListener('click', function() {
 		var desiredGrade = $('desiredGrade').value / 100;
 		var gradeValue = parseInt($('gradeValue').value);
-		var newTotalScore = gradeValue + totalPoints;
-		var minPoints = Math.ceil((desiredGrade * 
-			(totalPoints + gradeValue)) - acquiredPoints);
+		var minPoints;
+		
+		if (weighted) {
+			for (var key in processedStats) {
+				var category;
+				if ($(key + "minScoreButton").checked) {
+					category = key;
+				} else {
+					desiredGrade -= weightedGradeValues[key].gradePercentage
+				}
+			}
+
+			minPoints = desiredGrade / weightedGradeValues[category].weight
+			* (gradeValue + processedStats[category].totalPoints) - 
+			processedStats[category].myScore
+		} else {
+			var newTotalScore = gradeValue + totalPoints;
+			
+			minPoints = (desiredGrade * 
+				(totalPoints + gradeValue)) - acquiredPoints;
+		}
+
+		minPoints = Math.ceil(minPoints)
 		var minScore = Math.round(minPoints * 10000 / gradeValue)/100;
 		if (isNaN(minPoints)) {
 			$('minScoreRequired').innerHTML = "--";
